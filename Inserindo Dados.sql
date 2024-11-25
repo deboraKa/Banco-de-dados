3065b33f3de405a63552f28d9b9f6905b3ee817b
Débora-Karine-Cassimiro <61765035+deboraKa@users.noreply.github.com>
2024-11-25 13:02:37 -0300

    Add files via upload

---
+++ Inserindo Dados.sql
@@ -0,0 +1,15 @@
+-- Inserindo dados na tabela Departamentos
+INSERT INTO Departamentos (Nome, Localizacao) VALUES ('Recursos Humanos', 'São Paulo');
+INSERT INTO Departamentos (Nome, Localizacao) VALUES ('Tecnologia da Informação', 'Rio de Janeiro');
+INSERT INTO Departamentos (Nome, Localizacao) VALUES ('Operação ', 'Rio de Janeiro');
+INSERT INTO Departamentos (Nome, Localizacao) VALUES ('Administração ', 'São Paulo');
+-- Inserindo dados na tabela Funcionarios
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('João Silva', 30, 1);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Maria Clara', 28, 1);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Carlos Souza', 35, 2);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Julia', 33, 3);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Maria Oliveira', 28, 3);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Carlos Eduardo', 35, 4);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Suelen Oliveira', 28, 3);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Celio', 38, 2);
+INSERT INTO Funcionarios (Nome, Idade, DepartamentoID) VALUES ('Julio Mesquita', 48, 4);
